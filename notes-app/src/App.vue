b91d7de1a430d7744788548d3d71197629352afb
chance-stribling <cstribling1717@gmail.com>
2023-02-08 11:04:52 -0500

    pretty much down with this one for now

--- notes-app/src/App.vue
+++ notes-app/src/App.vue
@@ -14,8 +14,8 @@
 
   const addNote= () => {
     // adding validation
-    if(newNote.value.length <= 3 ){
-      alert("You need to have more than 3 characters.");
+    if(newNote.value.length <= 4 ){
+      alert("You need to have more than 4 characters.");
       return;
     }
     notes.value.push({
@@ -34,9 +34,9 @@
   <main>
     <div v-if="showModal" class="overlay">
       <div class="modal">
-        <textarea v-model="newNote" @keyup.ctrl.enter.exact="addNote" name="note" id="note" cols="30" rows="10"></textarea>
+        <textarea v-model.trim="newNote" @keyup.ctrl.enter.exact="addNote" name="note" id="note" cols="30" rows="10"></textarea>
         <button @click="addNote">Add Note</button>
-        <button @click="showModal = false" class="close">Close</button>
+        <button @click="showModal = false" class="close">Nevermind</button>
       </div>
     </div>
     
@@ -48,12 +48,9 @@
       </header>
       
       <div class="cards-container">
-        <div class="card" style="background-color: aquamarine;">
-          <p class="main-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam modi ab recusandae molestias? Earum, quidem?</p>
-          <p class="date">2/8/2023</p>
-        </div>
+        
         <div v-for="note in notes" :key="note.id" class="card" :style="{backgroundColor: note.backgroundColor}">
-          <p class="main-text">{{ note.text }}</p>
+          <p >{{ note.text }}</p>
           <p class="date">{{ note.date.toLocaleDateString("en-US") }} {{ note.date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}) }}</p>
         </div>
       </div>
@@ -66,6 +63,9 @@
   main{
     height: 100vh;
     width: 100vw;
+    background-color: #141414;
+    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;;
+
   }
   .container{
     max-width: 1000px;
@@ -76,12 +76,34 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    height: 90px;
+    padding-right: 10px;
+    padding-left: 10px;
+    /* background: linear-gradient(
+        90deg,
+        rgba(255, 0, 0, 1) 0%,
+        rgba(255, 154, 0, 1) 10%,
+        rgba(208, 222, 33, 1) 20%,
+        rgba(79, 220, 74, 1) 30%,
+        rgba(63, 218, 216, 1) 40%,
+        rgba(47, 201, 226, 1) 50%,
+        rgba(28, 127, 238, 1) 60%,
+        rgba(95, 21, 242, 1) 70%,
+        rgba(186, 12, 248, 1) 80%,
+        rgba(251, 7, 217, 1) 90%,
+        rgba(255, 0, 0, 1) 100%
+    ); */
+    background-color: rgb(230, 83, 78);
+    border-radius: 10px;
+    
+    
   }
   h1{
     font-weight: bold;
     margin-bottom: 25px;
     font-size: 70px;
-    color: black;
+    color: whitesmoke;
+    font-style: oblique;
   }
   header button{
     border: none;
@@ -89,9 +111,9 @@
     height: 50px;
     width: 50px;
     cursor: pointer;
-    background-color: black;
+    background-color: whitesmoke;
     border-radius: 100%;
-    color: white;
+    color: black;
     font-size: 25px;
   }
   .card{
@@ -104,8 +126,9 @@
     justify-content: space-between;
     margin-right: 20px;
     margin-bottom: 20px;
-    font-size: 16px;
+    font-size: large;
     font-weight: bold;
+    color: whitesmoke;
   }
   .date{
     font-size: 12.5px;
@@ -115,6 +138,7 @@
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
+    margin-top: 10px;
   }
   .overlay{
     position: absolute;
@@ -128,29 +152,34 @@
   }
   .modal{
     width: 750px;
-    background-color: white;
+    background-color: whitesmoke;
     border-radius: 10px;
     padding: 30px;
     position: relative;
     display: flex;
     flex-direction: column;
+    background-color: #282828;
   }
   .modal button{
     padding: 10px 20px;
     font-size: 20px;
     width: 100%;
-    background-color: violet;
+    background-color: aquamarine;
     border: none;
-    color: white;
+    color: Black;
     cursor: pointer;
     margin-top: 15px;
   }
   .modal .close{
     background-color: red;
     margin-top: 7px;
+    width: 50%;
+    align-self: center;
   }
   .modal textarea{
     font-size: 15px;
     font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;;
+    background-color: #282828;
+    color: whitesmoke;
   }
 </style>
